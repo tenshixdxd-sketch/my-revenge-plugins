988382aae42db4b5b3b2528063ffe1bf2ac4c971
tenshixdxd-sketch <tenshixdxd@gmail.com>
2026-04-23 14:10:44 +0500

    Update index.js

--- audiofix/index.js
+++ audiofix/index.js
@@ -1,20 +1,26 @@
-(function (t, e, a) {
-  "use strict";
-  const registry = e.ReactNative.TurboModuleRegistry;
-  const audioModule =
-    registry.get("NativeAudioManagerModule") ??
-    registry.get("RTNAudioManager") ??
-    registry.get("NativeAudioModule") ??
-    registry.get("AudioManager");
-  if (!audioModule) {
-    console.warn("[audiofix] Audio module not found");
-    return t;
+export default {
+  onLoad: () => {
+    const registry = window.ReactNative?.TurboModuleRegistry 
+      || bunny.metro.common.ReactNative.TurboModuleRegistry;
+    
+    const audioModule =
+      registry.get("NativeAudioManagerModule") ??
+      registry.get("RTNAudioManager") ??
+      registry.get("NativeAudioModule") ??
+      registry.get("AudioManager");
+
+    if (!audioModule) {
+      console.warn("[audiofix] Audio module not found");
+      return;
+    }
+
+    globalThis.__audiofix_unpatch = bunny.patcher.instead(
+      "setCommunicationModeOn",
+      audioModule,
+      () => {}
+    );
+  },
+  onUnload: () => {
+    globalThis.__audiofix_unpatch?.();
   }
-  const unpatch = a.instead("setCommunicationModeOn", audioModule, () => {});
-  t.onUnload = unpatch;
-  return t;
-})(
-  {},
-  bunny.metro.common,
-  bunny.patcher
-);
+};
